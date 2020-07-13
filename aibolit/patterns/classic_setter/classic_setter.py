15c62cffa6c1a2cc0b6a4d9639da2ff25eb1ed7f
Vitaly Protasov <izakladno@yandex.ru>
2020-07-13 18:10:02 +0300

    rewrited

--- aibolit/patterns/classic_setter/classic_setter.py
+++ aibolit/patterns/classic_setter/classic_setter.py
@@ -1,9 +1,7 @@
-from typing import List
-
-import javalang
-
+from typing import List, Set
 from aibolit.types_decl import LineNumber
-from aibolit.utils.ast_builder import build_ast
+from aibolit.ast_framework import ASTNodeType
+from aibolit.ast_framework.java_package import JavaPackage
 
 
 class ClassicSetter:
@@ -12,23 +10,19 @@ class ClassicSetter:
         pass
 
     def value(self, filename: str) -> List[LineNumber]:
-        lst: List[LineNumber] = []
-        tree = build_ast(filename).filter(javalang.tree.MethodDeclaration)
-        for _, node in tree:
-            if (node.return_type is None) and ('set' in node.name[:3]):
-                if (isinstance(node.body, list)) and len(node.body) < 2:
-                    for statement in node.body:
-                        if isinstance(statement, javalang.tree.StatementExpression):
-                            if isinstance(statement.expression, javalang.tree.Assignment):
-                                expression = statement.expression.expressionl
-                                if isinstance(expression, javalang.tree.This):
-                                    if statement.expression.type == '=':
-                                        if expression.selectors[0].member.lower() == node.name.lower()[3:]:
-                                            lst.append(node._position.line)
-                                    else:
-                                        break
-                                else:
-                                    break
-                            else:
-                                break
+        lst: Set[LineNumber] = []
+        ast = JavaPackage(filename)
+        method_decls = list(ast.get_nodes(ASTNodeType.METHOD_DECLARATION))
+        for node in method_decls:
+            method_name = ast.get_attr(node, 'name')
+            body_nodes = ast.get_attr(node, 'body')
+            if (ast.get_attr(node, 'return_type') is None) and method_name.startswith('set') and len(body_nodes) < 2:
+                for expr_child in ast.get_attr(node, 'body'):
+                    for child_this in ast.get_nodes(ASTNodeType.THIS):
+                        child_membref = ast.get_attr(child_this, 'selectors')
+                        if len(child_membref):
+                            referenced_name = ast.get_attr(child_membref[0].node_index, 'member')
+                            source_line = ast.get_attr(node, 'line')
+                            if method_name.lower()[3:] == referenced_name.lower() and source_line not in lst:
+                                lst.append(source_line)
         return lst
